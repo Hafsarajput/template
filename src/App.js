dd927c0dc198f68fed84c7b94702af87c8dcfdfd
Hafsarajput <144684038+Hafsarajput@users.noreply.github.com>
2023-12-20 10:41:37 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     
    <div className="App">
-      <Header/>
-        <Routes>
-        <Route path="/" element={<Hero />} />
-        <Route path='/Pr' element={<Hero />} />
-          <Route path='/Cardd' element={<Card />} />
-          <Route path = '/Contactt' element ={<Contact/>} />
-        </Routes>
+
+       
       
     </div>
   );
